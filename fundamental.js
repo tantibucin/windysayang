a62f9241c4f2253cc9321930544357ea1e3df4ed
tantibucin <146109424+tantibucin@users.noreply.github.com>
2023-09-26 22:08:14 +0700

    Add files via upload

---
+++ fundamental.js
@@ -0,0 +1,4 @@
+function bucin(){
+    alert("i love you sayaaangkuuu cintakuuu hatikuuuu kanjeng ratukuuu separuh nafaskuuuu. Beruntung kali aku bisa dapatin kamu, I love you to the moon and back. Kalau misalnya multiuniverse itu beneran ada, aku mau salah satu univers itu didalamnya ada kita akhirnya bersama. I see my future in your eyes habibti. I love youuuuu")
+    alert("Jangan selingkuh ya kamu")
+}
